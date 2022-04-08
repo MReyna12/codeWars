8a0cb6e82dde1b90fb32603a24e2dd32ad22c595
Michael <MReyna12@live.com>
2022-04-08 11:06:22 -0500

    two daily challenges

---
+++ challenges/8kyu/arrayPlusArray.js
@@ -0,0 +1,24 @@
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+// Function declaration; concat method; arrow function; reduce method
+function arrayPlusArray(arr1, arr2) {
+  const arrays = arr1.concat(arr2);
+  const sum = arrays.reduce((acc, current) => acc + current, 0)
+  return sum;
+}
+// Function declaration; one-liner; concat method; arrow function; reduce method
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((acc, current) => acc + current, 0);
+}
+
+// Function expression; concat method; arrow function; reduce method
+const arrayPlusArray = (arr1, arr2) => arr1.concat(arr2).reduce((acc, current) => acc + current, 0);
+
+// Function declaration (similar to above, but reduce the arrays separately)
+function arrayPlusArray(arr1, arr2) {
+  const sumOfArray1 = arr1.reduce((acc, current) => acc + current, 0);
+  const sumOfArray2 = arr2.reduce((acc, current) => acc + current, 0);
+  const sumOfArrays = sumOfArray1 + sumOfArray2;
+  return sumOfArrays;
+}
---
+++ challenges/8kyu/meanOfArray.js
@@ -0,0 +1,20 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+
+// Function declaration; arrow function; reduce method
+function getAverage(marks) {
+  return Math.floor(marks.reduce((acc, current) => (acc += current)) / marks.length);
+}
+
+// Function expression; arrow function; reduce method
+const getAverage = marks => Math.floor(marks.reduce((acc, current) => (acc += current)) / marks.length);
+
+// Function declaration; for loop
+function getAverage(marks) {
+  let grades = 0;
+  for (let i = 0; i < marks.length; i++) {
+    grades += marks[i]
+  }
+  return Math.floor(grades / marks.length);
+}
