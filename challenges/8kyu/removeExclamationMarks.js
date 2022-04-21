1f0971e4bbfb59f31ff055a365930b16cbbef7d8
Michael <MReyna12@live.com>
2022-04-21 09:25:53 -0500

    daily codewars challenges

---
+++ challenges/8kyu/removeExclamationMarks.js
@@ -0,0 +1,6 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// Function declaration; regex
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, '');
+}
