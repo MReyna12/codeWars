bbb21bee5000b8603f90f5201a20521630546848
Michael <MReyna12@live.com>
2022-04-23 12:31:25 -0500

    daily coding challenges

---
+++ challenges/8kyu/removingElements.js
@@ -0,0 +1,7 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// None of the arrays will be empty, so you don't have to worry about that!
+
+// Function declaration; filter method
+function removeEveryOther(arr) {
+ return arr.filter((_, i) => i % 2 === 0)
+}
