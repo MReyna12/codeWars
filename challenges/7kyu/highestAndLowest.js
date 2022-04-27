dffff8beb6fcff4f00e175096fb9574c65383ae7
Michael <MReyna12@live.com>
2022-04-27 10:15:46 -0500

    daily coding challenges

---
+++ challenges/7kyu/highestAndLowest.js
@@ -0,0 +1,21 @@
+//  In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+//  All numbers are valid Int32, no need to validate them.
+//  There will always be at least one number in the input string.
+//  Output string must be two numbers separated by a single space, and highest number is first.
+
+// Function declaration; split method; sort method
+function highAndLow(numbers) {
+  const array = numbers.split(' ');
+  const sortedArray = array.sort((a, b) => a - b);
+  const lowestNumber = Number(sortedArray[0]);
+  const highestNumber = Number(sortedArray[sortedArray.length - 1]);
+  return `${highestNumber} ${lowestNumber}`;
+}
+
+// Function declaration; split method; spread syntax; Math.min and Math.max to obtain the lowest and highest number in the array
+function highAndLow(numbers){
+  const array = numbers.split(' ');
+  const lowestNumber = Math.min(...array);
+  const highestNumber = Math.max(...array);
+  return `${highestNumber} ${lowestNumber}`;
+}
---
+++ challenges/7kyu/onesAndZeroes.js
@@ -0,0 +1,9 @@
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+
+// Function expression; join method
+const binaryArrayToNumber = arr => {
+  const string = arr.join('');
+  const integer = parseInt(string, 2)
+  return integer;
+}
