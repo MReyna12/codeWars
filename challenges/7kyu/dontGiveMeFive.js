00b94f32dea640da54a2e049d34c6a330ff923f1
Michael <MReyna12@live.com>
2022-05-09 08:54:37 -0500

    7kyu daily

---
+++ challenges/7kyu/dontGiveMeFive.js
@@ -0,0 +1,13 @@
+//  In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. 
+//  The start and the end number are both inclusive!
+//  The result may contain fives.
+//  The start number will always be smaller than the end number. Both numbers can be also negative!
+
+// function declaration
+function dontGiveMeFive(start, end) {
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum = String(i).includes('5') ? sum : sum + 1;
+  }
+  return sum
+}
