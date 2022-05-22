00c1ed38feff2d48e7acbf2d01ebf35931750d38
Michael <MReyna12@live.com>
2022-05-22 09:05:46 -0500

    another kyu

---
+++ challenges/8kyu/personalizedMessage.js
@@ -0,0 +1,8 @@
+//  Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+//  Use conditionals to return the proper message:
+//    name === owner // 'Hello boss'; otherwise // 'Hello guest'
+
+// Function declaration
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
