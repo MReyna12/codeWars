a98414549ad1f57469c3001b61ae561a804f8c55
Michael <MReyna12@live.com>
2022-06-09 10:31:29 -0500

    8kyu

---
+++ challenges/8kyu/appleTurnover.js
@@ -0,0 +1,6 @@
+function apple(x) {
+  const number = Number(x);
+  return number * number > 1000
+    ? "It's hotter than the sun!!"
+    : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
