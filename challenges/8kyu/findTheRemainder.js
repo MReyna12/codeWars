fb652dc58dc3bbfeffc9b688b5f8405bb3608050
Michael <MReyna12@live.com>
2022-06-09 10:28:24 -0500

    8kyu

---
+++ challenges/8kyu/findTheRemainder.js
@@ -0,0 +1,9 @@
+const remainder = (n, m) => {
+  const largestNumber = Math.max(n, m);
+  const smallestNumber = Math.min(n, m);
+  return largestNumber % smallestNumber;
+};
+
+const remainder = (n, m) => {
+  return a > b ? a % b : b % a;
+};
